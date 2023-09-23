8149ab76a8c790a0b768623d9f0c8f97a7f8fc57
Marwan Rasamny <mrasamny@desu.edu>
2023-09-23 16:39:19 +0000

    added test script

---
+++ testExplode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function cleanup()
+{
+    [ -e testFile ] && rm testFile
+}
+
+function error()
+{
+    echo -e "ERROR: $1"
+    cleanup
+    exit 1
+}
+
+[ -x explode ] || error "explode not found or not executable"
+
+declare -A stringTest
+stringTest["a"]="a"
+stringTest["ab"]="aab"
+stringTest["abc"]="aababc"
+
+for key in "${!stringTest[@]}"
+do
+    echo ${key} > testFile
+    actual=`./explode testFile`
+    expected="${key} --> ${stringTest[${key}]}"
+    [ "${actual}" != "${expected}" ] && error "\texpecting: ${expected}\n\t   actual: ${actual}"
+done
+
+cleanup
+
+exit 0
