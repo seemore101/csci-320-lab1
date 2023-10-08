0127856c0eff931a28ab0d99c02f1e4881fbcc85
seemore101 <82735897+seemore101@users.noreply.github.com>
2023-10-08 20:19:55 +0000

    lab1.c
    test

--- lab1.c
+++ lab1.c
@@ -3,3 +3,23 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lab1.h"
+
+
+char* mysteryExplode(const char* str){
+    int len = strlen(str);
+    char* newString = (char *)malloc((len * (len + 1) / 2) + 1);
+
+    int index = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j <= i; j++) {
+            newString[index++] = str[j];
+        }
+    }
+    newString[index] = '\0';
+
+    return newString;
+}
